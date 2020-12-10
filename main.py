735a908ce3f5948940f27e2ce22bb544546a10b1
InAction <ggutierrez@inaction.co>
2020-12-10 13:50:17 -0500

    Se sube proyecto con validaciones

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from db.reserva_db import ReservaInDB
 from db.reserva_db import update_reserva, get_reserva, database_reservas
 from models.reserva_models import ReservaIn, ReservaOut
+from datetime import datetime, date
 
 from fastapi import FastAPI, HTTPException
 
 api = FastAPI()
+consecutivo=5
+reserva_in_db:ReservaInDB
 
 @api.get("/reserva/vista/{num_reserva}")
 async def listar_reserva(num_reserva: str):
@@ -20,6 +23,53 @@ async def listar_reserva(num_reserva: str):
     return  reserva_out
 
 @api.post("/reserva/crear")
-async def crear_reserva(reserva:ReservaInDB):
-    database_reservas[reserva.num_reserva]=reserva
-    return reserva
+async def crear_reserva(nombre:str, identificacion:str, fecha_inicio:str, fecha_fin:str, tipo_habitacion:str):
+    global consecutivo
+
+
+    try:
+        fechai = datetime.strptime(fecha_inicio, '%d/%m/%Y')
+        fechaf = datetime.strptime(fecha_fin, '%d/%m/%Y')
+        
+    except:
+        raise HTTPException(status_code=422, detail="Formato de fecha inválido. debe ser dd/mm/aaaa")
+    
+    if(fechaf<fechai):
+        raise HTTPException(status_code=422, detail="La fecha final debe ser mayor que la fecha inicial")
+       
+
+    fecha=date.today()
+    
+    fecha_sys = datetime(
+    year=fecha.year, 
+    month=fecha.month,
+    day=fecha.day,
+    )
+    
+
+    if fechai<fecha_sys:
+        raise HTTPException(status_code=422, detail="La fecha inicial debe ser mayor a la fecha del sistema")
+
+    dias= (fechaf-fechai).days
+    valor=150000*dias
+    consecutivo+=1
+    if(consecutivo<10):
+        pre="rsrv0"
+    else:
+        pre ="rsrv"
+    idreserva=pre+str(consecutivo)
+       
+
+    reserva_in_db=ReservaInDB(**{"nombre":nombre,
+                            "identificacion":identificacion,
+                            "fecha":fecha.strftime('%d/%m/%Y'),
+                            "fecha_inicial":fecha_inicio,
+                            "fecha_final":fecha_fin,
+                            "tipo_hab":tipo_habitacion,
+                            "valor":valor,
+                            "num_reserva": idreserva})
+    
+    
+    database_reservas[reserva_in_db.num_reserva]=reserva_in_db
+
+    return "Se ha creado la reserva "+idreserva
